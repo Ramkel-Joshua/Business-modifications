04a39af1e1ba494ac997707daa844620ac141036
Ramkel Gabriel <ramkelgabriel1996@gmail.com>
2018-08-20 19:25:26 +0300

    Business Website

---
+++ script.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        $(".top-nav").addClass("light-header");
+    } else {
+        $(".top-nav").removeClass("light-header");
+    }
+});
+function myInfo(){
+    document.getElementById("text").innerHTML ="By Business Analyst!";
+}
